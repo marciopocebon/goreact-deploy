fdc0a48deffd756e51b2b4742534adf461f489ed
Diego Fernandes <diego.schell.f@gmail.com>
2018-05-26 19:29:19 -0300

    Configuração do saga e axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
+import sagas from './sagas';
 import reducers from './reducers';
+
+const middlewares = [];
+
+const sagaMiddleware = createSagaMiddleware();
+
+middlewares.push(sagaMiddleware);
 
 const createAppropriateStore =
   process.env.NODE_ENV === 'development' ? console.tron.createStore : createStore;
 
-const store = createAppropriateStore(reducers, compose(applyMiddleware(...[])));
+const store = createAppropriateStore(reducers, compose(applyMiddleware(...middlewares)));
+
+sagaMiddleware.run(sagas);
 
 export default store;
